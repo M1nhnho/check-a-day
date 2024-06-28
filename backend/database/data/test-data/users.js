92c0e92dcdd1dc9cb4efe8f9ae235186d58eb33d
M1nhnho <tien.nguho@hotmail.com>
2024-06-28 13:14:06 +0100

    Deleted an unnecessary bit from users test data.

--- backend/database/data/test-data/users.js
+++ backend/database/data/test-data/users.js
@@ -1,27 +1,23 @@
 module.exports = [
     {
-        user_id: '',
         username: '',
         email: '',
         password: '',
         avatar_url: ''
     },
     {
-        user_id: '',
         username: '',
         email: '',
         password: '',
         avatar_url: ''
     },
     {
-        user_id: '',
         username: '',
         email: '',
         password: '',
         avatar_url: ''
     },
     {
-        user_id: '',
         username: '',
         email: '',
         password: '',
